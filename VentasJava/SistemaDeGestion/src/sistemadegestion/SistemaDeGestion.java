b731957452d5345cac5b6d5ddee20e24d14fffd5
Nicole Alexandra Lara Anago <nalara1@espe.edu.ec>
2023-01-28 19:57:14 -0500

    Programa Terminado

---
+++ VentasJava/SistemaDeGestion/src/sistemadegestion/SistemaDeGestion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadegestion;
+
+
+import Vista.Login;
+import static sun.security.jgss.GSSUtil.login;
+
+
+
+/**
+ *
+ * @author Lana
+ */
+public class SistemaDeGestion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      Login lg = new Login();
+        lg.setVisible(true);
+    }
+    
+}
